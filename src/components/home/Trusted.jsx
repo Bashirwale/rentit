baadeaa6afd4a556ce4856823c7b605accd1d048
Abdullahi Bashir Wale <bashabdulbash@gmail.com>
2023-07-26 22:55:44 +0200

    bug

--- src/components/home/Trusted.jsx
+++ src/components/home/Trusted.jsx
@@ -6,16 +6,20 @@ import thewall from "../../components/assets/featured/thewall.png";
 
 const Trusted = () => {
   return (
-    <div className="w-full mt-6 overflow-hidden px-8">
+    <div className="w-full mt-6  px-8">
       <h2 className="text-base text-neutral-500 font-semibold text-center">
         Trusted By:
       </h2>
-      <div className="flex items-center justify-between md:justify-evenly mt-2">
-        <img src={techC} alt="trusted by" className="h-6 md:h-8" />
-        <img src={forbes} alt="trusted by" className="h-6 md:h-8" />
-        <img src={entrepreneur} alt="trusted by" className="h-6 md:h-8" />
-        <img src={techtimes} alt="trusted by" className="h-6 md:h-8" />
-        <img src={thewall} alt="trusted by" className="h-6 md:h-8" />
+      <div className="flex items-center justify-between md:justify-evenly mt-2 ">
+        <img src={techC} alt="trusted by" className="h-3 sm:h-6 md:h-8" />
+        <img src={forbes} alt="trusted by" className="h-3 sm:h-6 md:h-8" />
+        <img
+          src={entrepreneur}
+          alt="trusted by"
+          className="h-3 sm:h-6 md:h-8"
+        />
+        <img src={techtimes} alt="trusted by" className="h-3 sm:h-6 md:h-8" />
+        <img src={thewall} alt="trusted by" className="h-3 sm:h-6 md:h-8" />
       </div>
     </div>
   );
