842381e1495854bf8297d5f2d57faf5e02543880
Abdullahi Bashir Wale <bashabdulbash@gmail.com>
2023-07-29 01:57:05 +0200

    added new features

--- src/components/home/HowItWorks.jsx
+++ src/components/home/HowItWorks.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import interrior from "../../components/assets/interrior.png";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 const HowItWorks = () => {
+  const [showDetails, setShowDetails] = useState({
+    open1: false,
+    open2: false,
+    open3: false,
+    open4: false,
+  });
+
+  const handleToggleDetails = (divKey) => {
+    setShowDetails((prevDetails) => ({
+      ...prevDetails,
+      [divKey]: !prevDetails[divKey],
+    }));
+    console.log(showDetails.divkey);
+  };
   return (
     <div className="py-12 px-8 md:px-16">
       <h2 className="text-sm text-center md:text-left font-medium text-customPurple">
@@ -16,37 +33,175 @@ const HowItWorks = () => {
       </p>
       <img src={interrior} alt="interrior" className="w-full mt-4 md:hidden" />
       <div className="mt-8">
-        <div className="flex items-center gap-10 py-2 border-b-neutral-300">
-          <p className="text-base font-bold rounded-full bg-neutral-300 flex items-center justify-center p-2 w-8 h-8">
+        <div
+          className=" flex gap-4 sm:gap-6 md:gap-8 lg:gap-10 py-2 cursor-pointer"
+          onClick={() => handleToggleDetails("open1")}
+        >
+          <h3
+            className={
+              showDetails.open1
+                ? "text-base font-bold rounded-full bg-gradient-to-br from-blue-700 to-blue-400 text-white flex items-center justify-center p-2 w-8 h-8"
+                : "text-base font-bold rounded-full bg-neutral-300 flex items-center justify-center p-2 w-8 h-8"
+            }
+          >
             1.
-          </p>
-          <p className="text-base font-semibold text-neutral-500 border-b border-b-neutral-300 p-2 md:p-4 w-full">
-            Searched for Property
-          </p>
+          </h3>
+          <div className="border-b border-b-neutral-300 pb-2 w-full sm:w-4/5 md:w-3/4 lg:w-3/5">
+            <p
+              className={
+                showDetails.open1
+                  ? "flex items-center justify-between text-lg lg:text-2xl font-semibold bg-white bg-gradient-to-br from-blue-700 to-blue-400 bg-clip-text text-transparent "
+                  : "flex items-center justify-between text-base lg:text-2xl font-semibold text-neutral-500"
+              }
+            >
+              Searched for Property
+              <IoIosArrowDroprightCircle
+                className={showDetails.open1 ? "hidden" : "block"}
+              />
+              <IoIosArrowDropdownCircle
+                className={showDetails.open1 ? "block text-blue-600" : "hidden"}
+              />
+            </p>
+            <p
+              className={
+                showDetails.open1 ? "text-sm md:text-base mt-2" : "hidden"
+              }
+            >
+              Welcome to our apartment rental website! We are dedicated to
+              helping you find the perfect place to call home. Our selection of
+              properties includes a wide range of options to suit every need and
+              budget.
+            </p>
+          </div>
         </div>
-        <div className="flex items-center gap-10 py-2 ">
-          <p className="flex items-center justify-center text-base font-bold rounded-full bg-neutral-300 p-2 w-8 h-8">
+        <div
+          className="flex gap-4 sm:gap-6 md:gap-8 lg:gap-10 py-2"
+          onClick={() => handleToggleDetails("open2")}
+        >
+          <h3
+            className={
+              showDetails.open2
+                ? "text-base font-bold rounded-full bg-gradient-to-br from-purple-800 to-purple-400 text-white flex items-center justify-center p-2 w-8 h-8"
+                : "text-base font-bold rounded-full bg-neutral-300 flex items-center justify-center p-2 w-8 h-8"
+            }
+          >
             2.
-          </p>
-          <p className="text-base font-semibold text-neutral-500 border-b border-b-neutral-300 p-1 md:p-4 w-full">
-            Selected Wanted Apartment
-          </p>
+          </h3>
+          <div className="border-b border-b-neutral-300 pb-2 w-full sm:w-4/5 md:w-3/4 lg:w-3/5">
+            <p
+              className={
+                showDetails.open2
+                  ? "flex items-center justify-between text-lg lg:text-2xl font-semibold bg-white bg-gradient-to-br from-purple-800 to-purple-400 bg-clip-text text-transparent "
+                  : "flex items-center justify-between text-base lg:text-2xl font-semibold text-neutral-500"
+              }
+            >
+              Select Wanted Apartment
+              <IoIosArrowDroprightCircle
+                className={showDetails.open2 ? "hidden" : "block"}
+              />
+              <IoIosArrowDropdownCircle
+                className={
+                  showDetails.open2 ? "block text-purple-600" : "hidden"
+                }
+              />
+            </p>
+            <p
+              className={
+                showDetails.open2 ? "text-sm md:text-base mt-2" : "hidden"
+              }
+            >
+              Welcome to our apartment rental website! We are dedicated to
+              helping you find the perfect place to call home. Our selection of
+              properties includes a wide range of options to suit every need and
+              budget.
+            </p>
+          </div>
         </div>
-        <div className="flex items-center gap-10 py-2 border-b-neutral-500">
-          <p className="flex items-center justify-center text-base font-bold rounded-full bg-neutral-300 p-2 w-8 h-8">
+        <div
+          className="flex gap-4 sm:gap-6 md:gap-8 lg:gap-10 py-2 "
+          onClick={() => handleToggleDetails("open3")}
+        >
+          <h3
+            className={
+              showDetails.open3
+                ? "text-base font-bold rounded-full bg-gradient-to-br from-orange-700 to-orange-400 text-white flex items-center justify-center p-2 w-8 h-8"
+                : "text-base font-bold rounded-full bg-neutral-300 flex items-center justify-center p-2 w-8 h-8"
+            }
+          >
             3.
-          </p>
-          <p className="text-base font-semibold text-neutral-500 border-b border-b-neutral-300 p-2 md:p-4 w-full">
-            Schedule A Tour
-          </p>
+          </h3>
+          <div className="border-b border-b-neutral-300 pb-2 w-full sm:w-4/5 md:w-3/4 lg:w-3/5">
+            <p
+              className={
+                showDetails.open3
+                  ? "flex items-center justify-between text-lg lg:text-2xl font-semibold bg-white bg-gradient-to-br from-orange-800 to-orange-400 bg-clip-text text-transparent "
+                  : "flex items-center justify-between text-base lg:text-2xl font-semibold text-neutral-500"
+              }
+            >
+              Schedule A Tour
+              <IoIosArrowDroprightCircle
+                className={showDetails.open3 ? "hidden" : "block"}
+              />
+              <IoIosArrowDropdownCircle
+                className={
+                  showDetails.open3 ? "block text-orange-600" : "hidden"
+                }
+              />
+            </p>
+            <p
+              className={
+                showDetails.open3 ? "text-sm md:text-base mt-2" : "hidden"
+              }
+            >
+              Welcome to our apartment rental website! We are dedicated to
+              helping you find the perfect place to call home. Our selection of
+              properties includes a wide range of options to suit every need and
+              budget.
+            </p>
+          </div>
         </div>
-        <div className="flex items-center gap-10 py-2 border-b-neutral-300">
-          <p className="flex items-center justify-center text-base font-bold rounded-full bg-neutral-300 p-2 w-8 h-8">
+        <div
+          className="flex gap-4 sm:gap-6 md:gap-8 lg:gap-10 py-2 "
+          onClick={() => handleToggleDetails("open4")}
+        >
+          <h3
+            className={
+              showDetails.open4
+                ? "text-base font-bold rounded-full bg-gradient-to-br from-green-800 to-green-400 text-white flex items-center justify-center p-2 w-8 h-8"
+                : "text-base font-bold rounded-full bg-neutral-300 flex items-center justify-center p-2 w-8 h-8"
+            }
+          >
             4.
-          </p>
-          <p className="text-base font-semibold text-neutral-500 p-2 md:p-4">
-            Book Your Apartment
-          </p>
+          </h3>
+          <div className="border-b border-b-neutral-300 pb-2 w-full sm:w-4/5 md:w-3/4 lg:w-3/5">
+            <p
+              className={
+                showDetails.open4
+                  ? "flex items-center justify-between text-lg lg:text-2xl font-semibold bg-white bg-gradient-to-t from-green-800 to-green-400 bg-clip-text text-transparent "
+                  : "flex items-center justify-between text-base lg:text-2xl font-semibold text-neutral-500"
+              }
+            >
+              Book Your Apartment
+              <IoIosArrowDroprightCircle
+                className={showDetails.open4 ? "hidden" : "block"}
+              />
+              <IoIosArrowDropdownCircle
+                className={
+                  showDetails.open4 ? "block text-green-600" : "hidden"
+                }
+              />
+            </p>
+            <p
+              className={
+                showDetails.open4 ? "text-sm md:text-base mt-2" : "hidden"
+              }
+            >
+              Welcome to our apartment rental website! We are dedicated to
+              helping you find the perfect place to call home. Our selection of
+              properties includes a wide range of options to suit every need and
+              budget.
+            </p>
+          </div>
         </div>
       </div>
     </div>
